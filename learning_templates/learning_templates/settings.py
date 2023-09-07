c50661cf62ffdddeef6fc7d9c22d6d44958af2da
fasasimayokun <maykay.fash@gmail.com>
2023-09-07 18:43:20 +0000

    my commit for learning_templates

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -24,7 +24,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-#=oo^@$=(rcbxxrbi4efy8&wl28ti^z@9#uaw!5k$uu*bskq*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
